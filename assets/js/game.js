ad4f3eb0860f38dc28866db14c8328bce4261e5a
tooqk4u <tooqk4u@protonmail.com>
2022-08-15 15:24:50 -0400

    playerName variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-alert("this is an alert");
+// this creates a function named 'fight'
+
+let playerName = prompt('What is your Robots name?');
+
+// note there are no quotations around playerName
+
+console.log(playerName);
+
+console.log('This logs a string, good for leaving yourself a message');
+
+// this will do math and log 20
+
+console.log(10 + 10);
+
+// what is this?
+
+console.log('Our robots name is ' + playerName);
+
+function fight(){
+  alert('The fight has begun!');
+}
+fight();
